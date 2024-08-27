50d8e6bf71106395bd9d6efc403a64c67a08cb2f
Facu <francou.facundo@gmail.com>
2024-08-27 15:24:47 -0300

    test buil vercels

--- cliente-facturas-frontend/src/services/facturaService.js
+++ cliente-facturas-frontend/src/services/facturaService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const buscarFacturaPorCuit = async (cuit) => {
-    const response = await axios.get(`http://localhost:5000/api/facturas/${cuit}`);
+    const response = await axios.get(`http://181.98.176.80:5000/api/facturas/${cuit}`);
     console.log(response)
     return response.data;
     
